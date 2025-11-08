8258c42021bc698ce211c2cb4c9deb1852552f09
RebCeMe <rebemcecil@gmail.com>
2025-11-08 14:46:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* *** CAMBIO CLAVE: Agrega el basename *** */}
+    <BrowserRouter basename="/proyectolentesVR">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
